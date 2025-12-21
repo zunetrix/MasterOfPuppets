b6a044c4680980493d36677a67d4d55aff129cf7
Diego <allan_diego@hotmail.com>
2025-12-21 14:23:16 -0300

    remove unused lang

--- MasterOfPuppets/Resources/Language.Designer.cs
+++ MasterOfPuppets/Resources/Language.Designer.cs
@@ -60,417 +60,34 @@ internal class Language {
         }
     }
 
-    /// <summary>
-    ///   Looks up a localized string similar to {0} is not close enough to examine..
-    /// </summary>
-    internal static string ExamineErrorToast {
-        get {
-            return ResourceManager.GetString("ExamineErrorToast", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Click to link, Alt-click to examine, or right click to target..
-    /// </summary>
-    internal static string MainHelpExamine {
-        get {
-            return ResourceManager.GetString("MainHelpExamine", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Click to link or right click to target..
-    /// </summary>
-    internal static string MainHelpNoExamine {
-        get {
-            return ResourceManager.GetString("MainHelpNoExamine", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Targeting you.
-    /// </summary>
-    internal static string MainTargetingYou {
-        get {
-            return ResourceManager.GetString("MainTargetingYou", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Poll frequency in milliseconds.
-    /// </summary>
-    internal static string SettingsAdvancedPollFrequency {
-        get {
-            return ResourceManager.GetString("SettingsAdvancedPollFrequency", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Advanced.
-    /// </summary>
-    internal static string SettingsAdvancedTab {
-        get {
-            return ResourceManager.GetString("SettingsAdvancedTab", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Open examine window on Alt-click.
-    /// </summary>
-    internal static string SettingsBehaviourExamineEnabled {
-        get {
-            return ResourceManager.GetString("SettingsBehaviourExamineEnabled", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Focus target on hover.
-    /// </summary>
-    internal static string SettingsBehaviourFocusHover {
-        get {
-            return ResourceManager.GetString("SettingsBehaviourFocusHover", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Behaviour.
-    /// </summary>
-    internal static string SettingsBehaviourTab {
-        get {
-            return ResourceManager.GetString("SettingsBehaviourTab", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Log alliance members.
-    /// </summary>
-    internal static string SettingsFilterLogAlliance {
-        get {
-            return ResourceManager.GetString("SettingsFilterLogAlliance", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Log targeters engaged in combat.
-    /// </summary>
-    internal static string SettingsFilterLogCombat {
-        get {
-            return ResourceManager.GetString("SettingsFilterLogCombat", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Log party members.
-    /// </summary>
-    internal static string SettingsFilterLogParty {
-        get {
-            return ResourceManager.GetString("SettingsFilterLogParty", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Log yourself.
-    /// </summary>
-    internal static string SettingsFilterLogSelf {
-        get {
-            return ResourceManager.GetString("SettingsFilterLogSelf", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Filters.
-    /// </summary>
-    internal static string SettingsFilterTab {
-        get {
-            return ResourceManager.GetString("SettingsFilterTab", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Number of previous targeters to keep.
-    /// </summary>
-    internal static string SettingsHistoryAmount {
-        get {
-            return ResourceManager.GetString("SettingsHistoryAmount", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Show previous targeters.
-    /// </summary>
-    internal static string SettingsHistoryEnabled {
-        get {
-            return ResourceManager.GetString("SettingsHistoryEnabled", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Record history when window is closed.
-    /// </summary>
-    internal static string SettingsHistoryRecordClosed {
-        get {
-            return ResourceManager.GetString("SettingsHistoryRecordClosed", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to History.
-    /// </summary>
-    internal static string SettingsHistoryTab {
-        get {
-            return ResourceManager.GetString("SettingsHistoryTab", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Show timestamps.
-    /// </summary>
-    internal static string SettingsHistoryTimestamps {
-        get {
-            return ResourceManager.GetString("SettingsHistoryTimestamps", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Mark your target.
-    /// </summary>
-    internal static string SettingsMarkersMarkTarget {
-        get {
-            return ResourceManager.GetString("SettingsMarkersMarkTarget", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Target mark colour.
-    /// </summary>
-    internal static string SettingsMarkersMarkTargetColour {
-        get {
-            return ResourceManager.GetString("SettingsMarkersMarkTargetColour", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Mark targeting you.
-    /// </summary>
-    internal static string SettingsMarkersMarkTargeting {
-        get {
-            return ResourceManager.GetString("SettingsMarkersMarkTargeting", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Targeting mark colour.
-    /// </summary>
-    internal static string SettingsMarkersMarkTargetingColour {
-        get {
-            return ResourceManager.GetString("SettingsMarkersMarkTargetingColour", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Targeting mark size.
-    /// </summary>
-    internal static string SettingsMarkersMarkTargetingSize {
-        get {
-            return ResourceManager.GetString("SettingsMarkersMarkTargetingSize", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Target mark size.
-    /// </summary>
-    internal static string SettingsMarkersMarkTargetSize {
-        get {
-            return ResourceManager.GetString("SettingsMarkersMarkTargetSize", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Cooldown for sound (seconds).
-    /// </summary>
-    internal static string SettingsSoundCooldown {
-        get {
-            return ResourceManager.GetString("SettingsSoundCooldown", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Default.
-    /// </summary>
-    internal static string SettingsSoundDefaultDevice {
-        get {
-            return ResourceManager.GetString("SettingsSoundDefaultDevice", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Play sound when targeted.
-    /// </summary>
-    internal static string SettingsSoundEnabled {
-        get {
-            return ResourceManager.GetString("SettingsSoundEnabled", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Invalid device.
-    /// </summary>
-    internal static string SettingsSoundInvalidDevice {
-        get {
-            return ResourceManager.GetString("SettingsSoundInvalidDevice", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Output device.
-    /// </summary>
-    internal static string SettingsSoundOutputDevice {
-        get {
-            return ResourceManager.GetString("SettingsSoundOutputDevice", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Path to audio file.
-    /// </summary>
-    internal static string SettingsSoundPath {
-        get {
-            return ResourceManager.GetString("SettingsSoundPath", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Leave this blank to use a built-in sound..
-    /// </summary>
-    internal static string SettingsSoundPathHelp {
-        get {
-            return ResourceManager.GetString("SettingsSoundPathHelp", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Play sound when window is closed.
-    /// </summary>
-    internal static string SettingsSoundPlayWhenClosed {
-        get {
-            return ResourceManager.GetString("SettingsSoundPlayWhenClosed", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Sound.
-    /// </summary>
-    internal static string SettingsSoundTab {
-        get {
-            return ResourceManager.GetString("SettingsSoundTab", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Volume of sound.
-    /// </summary>
-    internal static string SettingsSoundVolume {
-        get {
-            return ResourceManager.GetString("SettingsSoundVolume", resourceCulture);
-        }
-    }
-
-
-
-    /// <summary>
-    ///   Looks up a localized string similar to Allow closing the main window with the escape key.
-    /// </summary>
     internal static string SettingsWindowAllowCloseWithEscape {
         get {
             return ResourceManager.GetString("SettingsWindowAllowCloseWithEscape", resourceCulture);
         }
     }
 
-    /// <summary>
-    ///   Looks up a localized string similar to Allow moving the main window.
-    /// </summary>
     internal static string SettingsWindowAllowMovement {
         get {
             return ResourceManager.GetString("SettingsWindowAllowMovement", resourceCulture);
         }
     }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Allow resizing the main window.
-    /// </summary>
     internal static string SettingsWindowAllowResize {
         get {
             return ResourceManager.GetString("SettingsWindowAllowResize", resourceCulture);
         }
     }
 
-    /// <summary>
-    ///   Looks up a localized string similar to Open on login.
-    /// </summary>
     internal static string SettingsWindowOpenLogin {
         get {
             return ResourceManager.GetString("SettingsWindowOpenLogin", resourceCulture);
         }
     }
 
-    /// <summary>
-    ///   Looks up a localized string similar to Show window while in combat.
-    /// </summary>
-    internal static string SettingsWindowShowCombat {
-        get {
-            return ResourceManager.GetString("SettingsWindowShowCombat", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Show settings button.
-    /// </summary>
     internal static string SettingsWindowShowConfigButton {
         get {
             return ResourceManager.GetString("SettingsWindowShowConfigButton", resourceCulture);
         }
     }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Show window while in cutscenes.
-    /// </summary>
-    internal static string SettingsWindowShowCutscene {
-        get {
-            return ResourceManager.GetString("SettingsWindowShowCutscene", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Show window while in instance.
-    /// </summary>
-    internal static string SettingsWindowShowInstance {
-        get {
-            return ResourceManager.GetString("SettingsWindowShowInstance", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Window.
-    /// </summary>
-    internal static string SettingsWindowTab {
-        get {
-            return ResourceManager.GetString("SettingsWindowTab", resourceCulture);
-        }
-    }
-
-    /// <summary>
-    ///   Looks up a localized string similar to Could not play sound file: {0}.
-    /// </summary>
-    internal static string SoundChatError {
-        get {
-            return ResourceManager.GetString("SoundChatError", resourceCulture);
-        }
-    }
-
-
-    // ------------------------------------------------------------------
-
-
 
     internal static string SettingsTitle {
         get {
@@ -690,6 +307,11 @@ internal class Language {
     internal static string ItemTitle {
         get {
             return ResourceManager.GetString("ItemTitle", resourceCulture);
+        }
+    }
+    internal static string GearSetTitle {
+        get {
+            return ResourceManager.GetString("GearSetTitle", resourceCulture);
         }
     }
     internal static string ShowMacroHelpBtn {
